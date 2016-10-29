26993b885eb79ba28332c75d41bc3f1ff4bbaa5a
Toan Ngo <xhunter9x@gmail.com>
2016-10-30 00:45:56 +0700

    seemingly done ordering

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,32 +4,20 @@ class OrdersController < ApplicationController
   end
 
   def create
-     @event = Event.find(params[:order][:event_id])
-       @order = Order.new(order_params)
+		@order = Order.new order_params
+		params[:quantity].each do |key, value|
+      order_details = OrderDetail.new(ticket_type_id: key, quantity: value)
+			redirect_to root_path
+		end
 
-       params[:quantity].each do |key, value|
-         order_details = OrderDetail.new(ticket_type_id: key, quantity: value)
-         order_details.ticket_type.max_quantity -= value.to_i
-         @order.order_details << order_details
-       end
+		if @order.save
+			flash[:success] = "Successfully booked"
+		else
+			flash[:error] = "Failed to booked"
+		end
+  end
 
-       begin
-         Order.transaction do
-           @order.save!
-           @order.order_details.each do |od|
-             od.ticket_type.save!
-           end
-         end
-
-         flash[:notice] = 'Order submitted. Thanks!'
-         redirect_to root_path
-       rescue
-         @order.errors.each do |key, value|
-           errors << "#{key} #{value}"
-         end
-
-         flash[:error] = errors.join("<br/>").html_safe
-         redirect_to new_event_order_path(build_params)
-       end
-     end
-   end
+  def order_params
+     params.require(:order).permit(:name, :phone, :address)
+  end
+end
