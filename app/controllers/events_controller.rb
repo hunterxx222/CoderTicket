5f6156a45347fe73ac0e7ed2ccdb8a8a943802d3
Toan Ngo <xhunter9x@gmail.com>
2016-10-29 14:40:30 +0700

    done showing and editing events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -27,21 +27,32 @@ class EventsController < ApplicationController
       end
   end
 
+  def edit
+    @event = Event.find(params[:id])
+
+    unless current_user.id == @event.user_id
+      flash[:error] = 'You must be the creator to edit this event'
+      redirect_to event_path(@event)
+    end
+  end
+
+  def update
+    @event = Event.find(params[:id])
+     if current_user.id != @event.user_id
+       flash[:error] = 'You must be the creator to edit this event'
+       redirect_to event_path(@event)
+     else @event.update(event_params)
+       flash[:notice] = 'Event updated'
+       redirect_to event_path(@event)
+     end
+  end
+
   def event_params
     params.require(:event).permit(:starts_at, :ends_at, :venue_id, :hero_image_url, :extended_html_description, :category_id, :name)
   end
 
-  def load_user
-    if params[:user_id]
-      @user = User.find params[:user_id]
-    else
-      @user = current_user
-    end
-  end
-
   def my_events
-    load_user
-    @event = Event.where(user_id: @user.id)
+    @events = Event.where(user_id: current_user.id)
   end
 
 end
